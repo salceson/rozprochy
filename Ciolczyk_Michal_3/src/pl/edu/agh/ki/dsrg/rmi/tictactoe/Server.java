7c103367bd70b29cfe052a575f09a6ef0ed5d8d1
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2015-04-23 22:46:55 +0200

    rmi corrections...

--- Ciolczyk_Michal_3/src/pl/edu/agh/ki/dsrg/rmi/tictactoe/Server.java
+++ Ciolczyk_Michal_3/src/pl/edu/agh/ki/dsrg/rmi/tictactoe/Server.java
@@ -3,9 +3,9 @@ package pl.edu.agh.ki.dsrg.rmi.tictactoe;
 import pl.edu.agh.ki.dsrg.rmi.tictactoe.broker.BoardBroker;
 import pl.edu.agh.ki.dsrg.rmi.tictactoe.broker.BoardBrokerImpl;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 /**
@@ -15,17 +15,17 @@ public class Server {
     static BoardBroker boardBroker;
 
     public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Usage: java Server <VISIBLE-HOST>");
+        if (args.length < 2) {
+            System.out.println("Usage: java Server <HOST> <PORT>");
             return;
         }
-        System.setProperty("java.rmi.server.hostname", args[0]);
+        String host = args[0];
+        String port = args[1];
         try {
             BoardBroker boardBrokerImpl = new BoardBrokerImpl();
             boardBroker = (BoardBroker) UnicastRemoteObject.exportObject(boardBrokerImpl, 0);
-            Registry registry = LocateRegistry.createRegistry(1099);
-            registry.rebind("broker", boardBroker);
-        } catch (RemoteException e) {
+            Naming.rebind("rmi://" + host + ":" + port + "/broker", boardBroker);
+        } catch (RemoteException | MalformedURLException e) {
             e.printStackTrace();
         }
     }
