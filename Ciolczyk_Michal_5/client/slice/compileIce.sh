32622f1347e0f17b5759bfe8bbb83b45d9ffcf8a
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2015-05-22 01:15:50 +0200

    Removed c++ client stub

--- Ciolczyk_Michal_5/client/slice/compileIce.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export ICE_INCLUDE_PATH="/usr/share/Ice-3.5.1/slice/Ice"
-export OPTIONS="-I ${ICE_INCLUDE_PATH}"
-
-slice2cpp --output-dir ../ Bank.ice ${OPTIONS}
-slice2cpp --output-dir ../ CertSigner.ice ${OPTIONS}
-slice2cpp --output-dir ../ FinancialNews.ice ${OPTIONS}
