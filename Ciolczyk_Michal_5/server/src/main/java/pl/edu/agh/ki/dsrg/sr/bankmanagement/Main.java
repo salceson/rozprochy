6809dadb772d638ba463f4686c3be24df891d677
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2015-05-21 21:57:12 +0200

    [zad5] Server - added bidirectional communication with Financial News Server.

--- Ciolczyk_Michal_5/server/src/main/java/pl/edu/agh/ki/dsrg/sr/bankmanagement/Main.java
+++ Ciolczyk_Michal_5/server/src/main/java/pl/edu/agh/ki/dsrg/sr/bankmanagement/Main.java
@@ -1,7 +1,9 @@
 package pl.edu.agh.ki.dsrg.sr.bankmanagement;
 
+import FinancialNews.*;
 import Ice.Communicator;
 import Ice.ObjectAdapter;
+import Ice.ObjectPrx;
 import Ice.ServantLocator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,14 +20,14 @@ public class Main {
     public static final String ACCOUNT_CATEGORY = "acc";
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
+    @SuppressWarnings("InfiniteLoopStatement")
     public static void main(String[] args) {
         Communicator communicator;
 
         communicator = initialize(args);
         LOGGER.info("Communicator initialized");
 
-
-        LOGGER.info("Creating object adapter");
+        LOGGER.info("Creating server object adapter");
 
         ObjectAdapter objectAdapter = communicator.createObjectAdapter("BankManager");
 
@@ -44,6 +46,44 @@ public class Main {
         LOGGER.info("Activated object adapter");
         LOGGER.info("Started server");
 
-        communicator.waitForShutdown();
+        Ice.ObjectPrx proxy = communicator.propertyToProxy("FinancialNews");
+        FinancialNewsServerPrx newsServer = FinancialNewsServerPrxHelper
+                .checkedCast(proxy);
+
+        Ice.ObjectAdapter clientAdapter = communicator.createObjectAdapter("");
+        FinancialNewsReceiver receiver =
+                new pl.edu.agh.ki.dsrg.sr.bankmanagement.ice.financialnews.FinancialNewsReceiver();
+        ObjectPrx objectProxy = clientAdapter.addWithUUID(receiver);
+        FinancialNewsReceiverPrx receiverProxy = FinancialNewsReceiverPrxHelper
+                .uncheckedCast(objectProxy);
+
+        clientAdapter.activate();
+
+        boolean tryToRegister = true;
+
+        while (true) {
+            try {
+                if (tryToRegister) {
+                    Ice.Connection connection = newsServer.ice_getConnection();
+                    connection.setAdapter(clientAdapter);
+                    newsServer.registerForNews(receiverProxy);
+                    LOGGER.info("Registered for financial notifications");
+                    tryToRegister = false;
+                } else {
+                    newsServer.ice_ping();
+                    Thread.sleep(1000);
+                }
+            } catch (Ice.SocketException | Ice.TimeoutException e) {
+                LOGGER.error("Financial notifications: connection error, trying to reconnect...");
+                tryToRegister = true;
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e1) {
+                    LOGGER.error("Thread interrupted: ", e1);
+                }
+            } catch (InterruptedException e) {
+                LOGGER.error("Thread interrupted: ", e);
+            }
+        }
     }
 }
